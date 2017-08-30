f9d447e54ce4a4a14726f651f1ac6e55c603a59a
vergil <vergil.sm@yandex.ru>
2017-08-30 16:16:28 +0300

    crerate comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :comments
+
   validates :user,     presence: true
 
   validates :title,    presence: true, length: {maximum: 255}
