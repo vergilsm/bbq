2e9663c47db2524e0445c98369f11e6bb5ace88e
vergil <vergil.sm@yandex.ru>
2017-11-22 18:38:11 +0300

    changed title to mailer

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -5,20 +5,20 @@ class EventMailer < ApplicationMailer
     @name  = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: I18n.t("mailers.event.new_subscription#{event.title}")
+    mail to: event.user.email, subject: I18n.t("mailers.event.new_subscription") + event.title
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: I18n.t("mailers.event.new_comment#{event.title}")
+    mail to: email, subject: I18n.t('mailers.event.new_comment') + event.title
   end
 
   def photo(event, photo, email)
     @photo = photo
     @event = event
 
-    mail to: email, subject: I18n.t("mailers.event.new_photo#{event.title}")
+    mail to: email, subject: I18n.t('mailers.event.new_photo') + event.title
   end
 end
